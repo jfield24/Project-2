1dd25371c1432a64a80191663b7ea45f5849ea98
tanvi-12 <69550520+tanvi-12@users.noreply.github.com>
2021-01-11 18:26:14 +1100

    pathing change

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, Response
+from flask import Flask, render_template, redirect, Response, url_for, request
 from flask_pymongo import PyMongo
 import run_api
 import json
